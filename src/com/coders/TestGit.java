dd70e185736605af853f5391f8abe5ec2db926a1
teachersmeng <teachercen@126.com>
2023-08-22 11:05:01 +0800

    第一次初始化

---
+++ src/com/coders/TestGit.java
@@ -0,0 +1,15 @@
+package com.coders;
+
+/**
+ * @project=git_home
+ * @package=com.coders
+ * @author= cenMengYuan
+ * @date=2023/8/22
+ * @Version=1.0
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("你好呀，兄弟们->Git");
+    }
+}
